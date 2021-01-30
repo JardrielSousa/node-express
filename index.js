71e2791f54319f691b65f4475f1429060cbde55b
jardriel sousa <jardriel.sousa@projeto.lead>
2021-01-30 00:05:36 -0300

    created of first end points get and post users

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const userRoute = require('./routes/userRoutes');
 const app = express();
 const port = 2000
+
+userRoute(app);
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{res.send('Olá mundo!!')})
 app.listen(port,()=>console.log('Api rodando na porta' + port));
